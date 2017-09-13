0a5bcb18bbb9a71b22a769b10f68a26de9d13bcb
websitekay <websitekay@gmail.com>
2017-09-13 05:26:59 -0700

    move html to jsx

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,16 +6,72 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+class Hello extends React.Component {
+  render() {
+    return (
+    <div className="App">
+      <header>
+        <h1>RSVP</h1>
+        <p>A Treehouse App</p>
+        <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </header>
+      <div className="main">
+        <div>
+          <h2>Invitees</h2>
+          <label>
+            <input type="checkbox" /> Hide those who haven't responded
+          </label>
+        </div>
+        <table className="counter">
+          <tbody>
+            <tr>
+              <td>Attending:</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>Unconfirmed:</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+        <ul>
+          <li className="pending"><span>Safia</span></li>
+          <li className="responded"><span>Iver</span>
+            <label>
+              <input type="checkbox" checked /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+          </li>
+          <li className="responded">
+            <span>Corrina</span>
+            <label>
+              <input type="checkbox" checked /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+          </li>
+          <li>
+            <span>Joel</span>
+            <label>
+              <input type="checkbox" /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+          </li>
+        </ul>
+      </div>
+    </div>
 
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
+    );
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
